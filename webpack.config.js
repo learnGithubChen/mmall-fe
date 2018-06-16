623977582f4385e45d5f5d8da4f386e2d9b5ceb1
learnGithubChen <729245768@qq.com>
2018-06-16 21:37:02 +0800

    修改网址

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ var config = {
     },
     output: {
         path        : __dirname + '/dist/',
-        publicPath  : 'dev' === WEBPACK_ENV ? '/dist/' : '//s.happymmall.com/mmall-fe/dist/',
+        publicPath  : 'dev' === WEBPACK_ENV ? '/dist/' : '//s.happymall.com/mmall-fe/dist/',
         filename    : 'js/[name].js'
     },
     externals : {
